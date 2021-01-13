e0cedbaa4f90c4eef42c247d2cf376daa4321f6d
Graham Preston <graham.preston@appfolio.com>
2021-01-13 15:32:40 -0800

    Valid and invalid image upload tests

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -7,11 +7,17 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'form', 1
   end
 
-  test 'uploading image should redirect to image URL' do
-    post images_url, params: { image: { link: 'https://example.com' } }
+  test 'uploading valid URL should work and redirect' do
+    assert_difference('Image.count', 1) do
+      post images_url, params: { image: { link: 'https://www.example.com' } }
+    end
     assert_redirected_to image_url(Image.last.id)
-    follow_redirect!
-    assert_response :success
-    assert_select 'img', 1
+  end
+
+  test "don't upload invalid URL" do
+    assert_no_difference 'Image.count' do
+      post images_url, params: { image: { link: 'bad link' } }
+    end
+    assert_response :unprocessable_entity
   end
 end
