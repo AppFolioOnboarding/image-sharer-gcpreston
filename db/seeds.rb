476e84b0cdd3732c780abb995a95ddd2d8f1be15
Graham Preston <graham.preston@appfolio.com>
2021-01-13 17:36:10 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Image.create(
+  [
+    { link: 'https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg' },
+    { link: 'https://i.redd.it/slfsbi0q05b61.jpg' },
+    { link: 'https://i.imgur.com/aXWW2FY.jpg' },
+    { link: 'https://i.imgur.com/xFa5MHC.jpg' },
+    { link: 'https://i.imgur.com/g8saqz0.jpeg' },
+    { link: 'https://i.imgur.com/BFGps8c.jpg' },
+    { link: 'https://i.imgur.com/7KbkTlr.jpg' },
+    { link: 'https://i.imgur.com/pn9D8Zs.jpeg' },
+    { link: 'https://i.imgur.com/65ZSHES.jpeg' },
+    { link: 'https://i.imgur.com/7MCRjeZ.jpg' },
+    { link: 'https://i.imgur.com/B5M9TMc.jpeg' },
+    { link: 'https://i.imgur.com/kVQ2jok.jpeg' },
+    { link: 'https://i.imgur.com/3zYSpb2.jpeg' },
+    { link: 'https://i.imgur.com/2VNnNd5.jpg' },
+    { link: 'https://i.imgur.com/Ksv7k6r.jpeg' },
+    { link: 'https://i.imgur.com/sGTUir6.jpg' },
+    { link: 'https://i.imgur.com/K0Xw2tx.jpeg' },
+    { link: 'https://i.imgur.com/kdoHZ7r.jpg' },
+    { link: 'https://i.imgur.com/tL80jRS.jpg' },
+    { link: 'https://i.imgur.com/2bvab7y.jpg' },
+    { link: 'https://i.redd.it/8arh4bwb05b61.jpg' }
+  ]
+)
