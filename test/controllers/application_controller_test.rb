3e3b084bdf098304a43930b3e56922d3ed40c53c
Graham Preston <graham.preston@appfolio.com>
2021-01-13 16:50:54 -0800

    Add image index to homepage + test

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -6,4 +6,16 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
     assert_select 'h1', 'Welcome to Ropes!'
   end
+
+  test 'image index descending order' do
+    img1 = Image.create(link: 'https://example.com')
+    img2 = Image.create(link: 'https://example.com/thing')
+
+    get root_path
+    assert_select 'img' do |images|
+      assert_equal images.length, Image.count
+      assert_equal images[0]['src'], img2.link
+      assert_equal images[1]['src'], img1.link
+    end
+  end
 end
