b8aece2375571d5d4e68ebaf4578912aca71bb23
Graham Preston <graham.preston@appfolio.com>
2021-01-15 14:31:56 -0800

    Add footer

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Container } from 'reactstrap';
+import Footer from './Footer';
 import Header from './Header';
 
 export default function App() {
   return (
-    <div>
+    <Container>
       <Header title="Tell us what you think" />
       Put your components here: Flash Message, Form, Footer
     </div>
+      <Footer text="Copyright: Appfolio Inc. Onboarding" />
+    </Container>
   );
 }
 
