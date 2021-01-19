435eabb2ca09b54cddd4533298009122d0e397b3
Graham Preston <graham.preston@appfolio.com>
2021-01-19 15:22:27 -0800

    Track form data in state, send POST to server

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import { post } from '../utils/helper';
 
 export default function FeedbackForm() {
+  const [name, setName] = useState('');
+  const [comments, setComments] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    post('/api/feedbacks', { name, comments });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Label for="name">Your name:</Label>
-        <Input type="text" name="name" id="name" />
+        <Input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="comments">Comments:</Label>
-        <Input type="textarea" name="comments" id="comments" />
+        <Input
+          type="textarea"
+          name="comments"
+          id="comments"
+          value={comments}
+          onChange={e => setComments(e.target.value)}
+        />
       </FormGroup>
       <Button type="submit" color="primary">Submit</Button>
     </Form>
