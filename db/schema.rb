151b79d3d65590ef7a86f150b3d50636341058b3
Graham Preston <graham.preston@appfolio.com>
2021-01-19 15:23:29 -0800

    Feedback model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_172509) do
+ActiveRecord::Schema.define(version: 2021_01_19_231212) do
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "name"
+    t.string "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "link"
